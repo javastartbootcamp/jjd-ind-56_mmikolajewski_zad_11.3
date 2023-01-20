6b1c2f872f49eb464c55b02a6af8926c2d60d62e
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-01-20 11:54:16 +0000

    Rozwiązanie 1;

--- src/main/java/pl/javastart/task/logic/Component.java
+++ src/main/java/pl/javastart/task/logic/Component.java
@@ -3,37 +3,13 @@ package pl.javastart.task.logic;
 import java.util.InputMismatchException;
 
 public abstract class Component {
-    private String name;
-    private String producer;
-    private String id;
+    String name;
+    String producer;
+    String id;
 
     public Component(String name, String producer, String id) {
         this.name = name;
         this.producer = producer;
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getProducer() {
-        return producer;
-    }
-
-    public void setProducer(String producer) {
-        this.producer = producer;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
         this.id = id;
     }
 
